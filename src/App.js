4ae5dfd0543e825525f4b3a786a12574e6ec17f2
Loïc Lissens <loiclissens@gmail.com>
2020-03-06 10:42:13 +0100

    intégration metronome et style

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import { Route, BrowserRouter as Router, Link, useRouteMatch } from "react-route
 import "./css/App.min.css";
 import Metronome from "./metronome";
 
+import metronomeImg from "./assets/metronome.svg";
+import systemeImg from "./assets/systeme.svg";
+import loveImg from "./assets/love.svg";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Le bon Workshop !</h1>
+        <h1>
+          React + Electron = <img src={loveImg} />
+        </h1>
       </header>
       <main>
-        <Metronome />
+        <Router>
+          <nav className="navbar">
+            <div className="container-link">
+              <Link className="nav-link" to="/metronome">
+                Metronome
+                <img src={metronomeImg} />
+              </Link>
+            </div>
+            <div className="container-link">
+              <Link className="nav-link" to="/systemespecs">
+                Systemespecs
+                <img src={systemeImg} />
+              </Link>
+            </div>
+          </nav>
+          <Route path="/metronome">
+            <Metronome />
+          </Route>
+        </Router>
       </main>
-      <footer>Workshop by Denis Tété & lolo</footer>
     </div>
   );
 }
