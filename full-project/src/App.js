598e46d742c2447df5c06c3538f4b11d461f8273
Loïc Lissens <loiclissens@gmail.com>
2020-03-09 11:34:04 +0100

    init

--- full-project/src/App.js
+++ full-project/src/App.js
@@ -9,39 +9,39 @@ import systemeImg from "./assets/systeme.svg";
 import loveImg from "./assets/love.svg";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>
-                    React + Electron = <img src={loveImg} alt="Spread the love" />
-                </h1>
-            </header>
-            <main>
-                <Router>
-                    <nav className="navbar">
-                        <div className="container-link">
-                            <Link className="nav-link" to="/metronome">
-                                Metronome
-                                <img src={metronomeImg} alt="this looks cool," />
-                            </Link>
-                        </div>
-                        <div className="container-link">
-                            <Link className="nav-link" to="/systemespec">
-                                Systemespecs
-                                <img src={systemeImg} alt="doesn't it?" />
-                            </Link>
-                        </div>
-                    </nav>
-                    <Route path="/metronome">
-                        <Metronome />
-                    </Route>
-                    <Route path="/systemespec">
-                        <SystemSpecs />
-                    </Route>
-                </Router>
-            </main>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          React + Electron = <img src={loveImg} alt="Spread the love" />
+        </h1>
+      </header>
+      <main>
+        <Router>
+          <nav className="navbar">
+            <div className="container-link">
+              <Link className="nav-link" to="/metronome">
+                Metronome
+                <img src={metronomeImg} alt="this looks cool," />
+              </Link>
+            </div>
+            <div className="container-link">
+              <Link className="nav-link" to="/systemespec">
+                Systemespecs
+                <img src={systemeImg} alt="doesn't it?" />
+              </Link>
+            </div>
+          </nav>
+          <Route path="/metronome">
+            <Metronome />
+          </Route>
+          <Route path="/systemespec">
+            <SystemSpecs />
+          </Route>
+        </Router>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- full-project/src/menu.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-
-const Menu = () => {
-  return <h1>Workshop by Dede Tété & Lolo</h1>;
-};
-export default Menu;
